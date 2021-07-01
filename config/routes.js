789f7e0ee755742a3e99ab8d9b1e0df8ef0fae91
Gaston Gouveia <gastongouveia@gmail.com>
2021-07-01 00:40:27 -0300

    autenticação nas rotas

--- config/routes.js
+++ config/routes.js
@@ -4,20 +4,25 @@ module.exports = app => {
     app.post('/validateToken', app.api.auth.validateToken)
 
     app.route('/users')
+        .all(app.config.passport.authenticate())
         .post(app.api.user.save)
         .get(app.api.user.get)
 
     app.route('/users/:id')
+        .all(app.config.passport.authenticate())
         .put(app.api.user.save)
         .get(app.api.user.getById)
 
     app.route('/articles')
         .get(app.api.articles.get)
+        .post(app.config.passport.authenticate())
         .post(app.api.articles.save)
 
     app.route('/articles/:id')
         .get(app.api.articles.getById)
+        .put(app.config.passport.authenticate())
         .put(app.api.articles.save)
+        .delete(app.config.passport.authenticate())
         .delete(app.api.articles.remove)
     
 }
